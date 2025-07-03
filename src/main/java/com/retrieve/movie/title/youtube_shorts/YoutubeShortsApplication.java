541bc0c05b1f37e041f3894e190cf9c79a2872ee
Paul Albu <paul.i.albu@gmail.com>
2025-07-03 10:31:30 +0300

    initial commit

---
+++ src/main/java/com/retrieve/movie/title/youtube_shorts/YoutubeShortsApplication.java
@@ -0,0 +1,13 @@
+package com.retrieve.movie.title.youtube_shorts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeShortsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeShortsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retrieve/movie/title/youtube_shorts/YoutubeShortsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retrieve.movie.title.youtube_shorts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeShortsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
